4d91a19b9c343f3cc4bc04ae1ace47eae5b377ba
Дмитрий Муратов <myratov91@yandex.ru>
2023-02-04 12:19:11 +0300

    delete comments

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -9,27 +9,12 @@ class PepSpider(scrapy.Spider):
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        all_pep = response.xpath('//section[@id="numerical-index"]')
+        all_pep = response.css('section#numerical-index')
         tbody = all_pep.css('tbody')
         href = tbody.css('a::attr(href)')
         for link in href:
             yield response.follow(link, callback=self.parse_pep)
 
-    # Давай здесь лучше воспользуемся встроенными инструментами в модуль,
-    # а не будем использовать сплит,
-    # а потом делать срезы. Здесь можно поступить, например, вот так:
-        # Получить таблицу.
-        # И после этого получить number подобным образом:
-        # table.css('dt:contains("PEP") + dd::text').get()
-        # Тоже самое сделаем и для статуса.
-
-    # Дмитрий, здравствуйте, не смог найти Вас в "пачке", решил написать тут.
-    # Я не очень понял какую именно таблицу брать?Которая на странице с PEP-ом?
-    # или с главной странице, где полный список PEP-ов?
-    # я не смог имплементировать код именно так как писали вы выше.
-    # И поняв что главное избавиться от метода split() и срезов
-    # сделал чуть подругому,сгенерировал "регулярки" через ChatGPT
-    # "прекрутил" их к коду и получился тот же результат.
     def parse_pep(self, response):
         number = response.css('h1.page-title').re(r'<h1.*?>(PEP )(\d{1,4})')
         data = {
